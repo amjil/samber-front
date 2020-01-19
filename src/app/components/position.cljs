72c15ddfeef7be46bd827b8c51d630f7b98cc8fe
amjil <amjilt@aliyun.com>
2020-01-19 13:49:30 +0800

    position

--- src/app/components/position.cljs
+++ src/app/components/position.cljs
@@ -1,12 +1,12 @@
-(ns app.components.position)
+(ns app.components.position
+  (:require [app.util.quill :as quill-util]
+            [re-frame.core :refer [dispatch subscribe]]))
 
 (def properties
   [ "direction"  ;; RTL support
     "box-sizing"
     "width"  ;; on Chrome and IE exclude the scrollbar so the mirror div wraps exactly as the textarea does
     "height"
-    "top"
-    "left"
     "overflow-x"
     "overflow-y"  ;; copy the scrollbar for IE
 
@@ -51,13 +51,15 @@
         computed (js/window.getComputedStyle e)
         span (js/document.createElement "span")]
     (set! (.-id div) "input-textarea-caret-position-mirror-div")
-    (js/document.body.prependChild div)
+    (js/document.body.appendChild div)
     (doall (map #(aset (.-style div) % (.getPropertyValue computed %)) properties))
     (doseq [[k v] {"writing-mode" "vertical-lr"
                    "position" "absolute"
                    "visibility" "hidden"
                    "font-family" "Mongolian White"
-                   "white-space" "pre-wrap"}]
+                   "white-space" "pre-wrap"
+                   "top" "3.2rem"
+                   "left" ".7rem"}]
       (aset (.-style div) k v))
 
     (if is-firefox
@@ -66,9 +68,11 @@
       (aset (.-style div) "overflow" "hidden"))
 
     (set! (.-innerHTML div) innter-html)
-    (set! (.-textContent span) "hahah")
+    (set! (.-textContent span) ".")
 
-    (.appendChild div span)
+    (if (= 1 (.-length (.-children div)))
+      (.appendChild (aget (.-children div) 0) span)
+      (.appendChild div span))
 
     ;; [top left height]
     [(+ (.-offsetTop span) (js/parseInt (.getPropertyValue computed "border-top-width")))
@@ -77,3 +81,10 @@
 
 (defn editor-div []
   (js/document.querySelector ".ql-editor"))
+
+(defn index [idx]
+  (let [div (editor-div)
+        quill @(subscribe [:quill])
+        quill-delta (quill-util/delta quill idx)
+        html-content (quill-util/delta-to-html quill-delta)]
+    (coordinates div html-content)))
