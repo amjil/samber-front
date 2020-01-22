f226cf5acb8c8ca156a341fbdf2e2e9a952b2782
amjil <amjilt@aliyun.com>
2020-01-23 00:08:45 +0800

    improve onscroll

--- src/app/components/caret.cljs
+++ src/app/components/caret.cljs
@@ -1,11 +1,8 @@
 (ns app.components.caret)
 
-(defn index [coord]
-  (let [x (first coord)
-        y (last coord)
-        caret-div (js/document.getElementById "caret-position-div")
+(defn index [range]
+  (let [caret-div (js/document.getElementById "caret-position-div")
         div (if caret-div caret-div (js/document.createElement "div"))
-        range (js/document.caretRangeFromPoint x y)
         cloned-range (.cloneRange range)
         shadow-caret (js/document.createTextNode "|")]
     (if (> (- (.-endOffset range) 1) 0)
@@ -16,7 +13,7 @@
         (.insertNode cloned-range shadow-caret)
         (.selectNode cloned-range shadow-caret)))
     (let [bound (.getBoundingClientRect cloned-range)]
-      ;; Sometimes when wrapping get zero
+      ;; Sometimes when wrapping get zero bound
       (when (= (.-left bound) 0)
         (.insertNode cloned-range shadow-caret)
         (.selectNode cloned-range shadow-caret))
@@ -32,5 +29,6 @@
 
     (when-not caret-div
       (set! (.-id div) "caret-position-div")
-      (js/document.body.appendChild div))
+      (let [quill-wrapper (js/document.getElementById "quill-editor-my-editor-id")]
+        (.appendChild quill-wrapper div)))
     (.detach cloned-range)))
